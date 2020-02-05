999b6cd7199ae6db3229c9f91d1085d891c6c907
Tom Hartley <tom@tomhrtly.com>
2020-02-05 14:21:13 +0000

    Remove data property

--- src/WPQuery.js
+++ src/WPQuery.js
@@ -39,8 +39,8 @@ class WPQuery {
 
         return new Promise((resolve, reject) => {
             axios[method](this._baseURL + string)
-                .then((response) => resolve(response.data))
-                .catch((error) => reject(error.response.data));
+                .then((response) => resolve(response))
+                .catch((error) => reject(error.response));
         });
     }
 
