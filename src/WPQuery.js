c9f539a9f0d372f9aa8eecf69150e90293d95635
Tom Hartley <tom@tomhrtly.com>
2020-03-22 09:56:11 +0000

    Use object for post data

--- src/WPQuery.js
+++ src/WPQuery.js
@@ -14,31 +14,36 @@ class WPQuery {
     }
 
     request(method, resource = 'posts', params = {}, config = {}) {
-        const globals = ['fields', 'embed', 'method', 'envelope', 'jsonp'];
+        const data = params;
         let string = `/wp-json/wp/v2/${resource}`;
 
-        if (params.id) {
+        if (data.id) {
             string += `/${params.id}/`;
         }
 
-        string += '?';
+        if (method === 'get') {
+            const globals = ['fields', 'embed', 'method', 'envelope', 'jsonp'];
+            string += '?';
 
-        Object.keys(params).forEach((element) => {
-            let param = element.replace(/\.?([A-Z]+)/g, (x, y) => `_${y.toLowerCase()}`).replace(/^_/, '');
+            Object.keys(data).forEach((element) => {
+                let param = element
+                    .replace(/\.?([A-Z]+)/g, (x, y) => `_${y.toLowerCase()}`)
+                    .replace(/^_/, '');
 
-            if (globals.includes(element)) {
-                param = `_${param}`;
-            }
+                if (globals.includes(element)) {
+                    param = `_${param}`;
+                }
 
-            if (Array.isArray(params[element])) {
-                string += `&${param}=${params[element].join()}`;
-            } else if (element !== 'id') {
-                string += `&${param}=${params[element]}`;
-            }
-        });
+                if (Array.isArray(params[element])) {
+                    string += `&${param}=${params[element].join()}`;
+                } else if (element !== 'id') {
+                    string += `&${param}=${params[element]}`;
+                }
+            });
+        }
 
         return new Promise((resolve, reject) => {
-            axios[method](this._baseURL + string, {}, config)
+            axios[method](this._baseURL + string, data, config)
                 .then((response) => resolve(response))
                 .catch((error) => reject(error.response));
         });
